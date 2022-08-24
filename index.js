c8dd23bbb4a56199fa5de8530705855d28b7f3dd
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-08-24 18:47:19 +0200

    Start out site

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const { home } = require('./controllers/home');
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true}));
 app.use('/static', express.static('static'));
+
+app.get('/', home);
 
 app.listen(3000, () => console.log('All good to server'))
