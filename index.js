3383a5548b162c5d47bfc4d2a9380f3be01a7fc4
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-08-24 18:36:29 +0200

    Start my server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+    extname: '.hbs'
+}).engine);
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true}));
+app.use('/static', express.static('static'));
+
+app.listen(3000, () => console.log('All good to server'))
